cb7450801340c2834a80801b72fbe4448b9674c4
Dalton Chichester <dchiches@mail.umw.edu>
2020-01-04 12:11:25 -0500

    Added ProblemB.java as well as 3 test cases

---
+++ Problem B/ProblemB.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * The second problem in the HS programming constest official solution code.
+ *
+ * @author Dalton Chichester
+ * @version 1.0.0
+ */
+public class ProblemB
+{
+    public static void main(String[] args) throws IOException
+    {
+    	int pairs;
+        int giraffeA;
+        int giraffeB;
+        int[] giraffeTotals;
+    	Scanner scnr = new Scanner(System.in);
+
+		System.out.print("Please input how many pairs of giraffes that Aunt Gingerbread has counted: ");
+        pairs = scnr.nextInt();
+        scnr.nextLine();
+
+        giraffeTotals = new int[pairs];
+
+        for(int i = 0; i < pairs; i++)
+        {
+            int pairCount = i + 1;
+            System.out.print("Please input how many spots were on giraffe 1, pair " + pairCount + ": ");
+            giraffeA = scnr.nextInt();
+            scnr.nextLine();
+
+            System.out.print("Please input how many spots were on giraffe 2, pair " + pairCount + ": ");
+            giraffeB = scnr.nextInt();
+            scnr.nextLine();
+
+            giraffeTotals[i] = giraffeA + giraffeB;
+        }
+
+        for(int i = 0; i < pairs; i++)
+        {
+            System.out.println(giraffeTotals[i]);
+        }
+    }
+}
