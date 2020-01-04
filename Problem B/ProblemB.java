f208b63ca3cb1517ac3f605afb9090e15c19e94e
Dalton Chichester <dchiches@mail.umw.edu>
2020-01-04 12:58:28 -0500

    Added ProblemC.java as well as 5 test cases

--- Problem B/ProblemB.java
+++ Problem B/ProblemB.java
@@ -9,7 +9,7 @@ import java.util.*;
  */
 public class ProblemB
 {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args)
     {
     	int pairs;
         int giraffeA;
---
+++ Problem C/ProblemC.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * The thrid problem in the HS programming constest official solution code.
+ *
+ * @author Dalton Chichester
+ * @version 1.0.0
+ */
+public class ProblemC
+{
+    public static void main(String[] args)
+    {
+    	int facetimeCalls;
+        double rateOfGrowth = 0.5;
+        int jessiesHeight = 3;
+        double jessiesNewHeight;
+		double jessiesNewHeightInches;
+    	Scanner scnr = new Scanner(System.in);
+
+		System.out.print("Please input how many FaceTime Calls there has been: ");
+        facetimeCalls = scnr.nextInt();
+        scnr.nextLine();
+
+        jessiesNewHeight = jessiesHeight + (rateOfGrowth * facetimeCalls);
+        jessiesNewHeightInches = jessiesNewHeight % 1;
+        jessiesNewHeight = jessiesNewHeight - jessiesNewHeightInches;
+
+        if(jessiesNewHeight > 19)
+        {
+        	if(jessiesNewHeightInches == 0.5)
+        	{
+        		System.out.printf("Jessie’s height is %.0f and 1 half feet!\n", jessiesNewHeight);
+        	}
+        	else
+        	{
+        		System.out.printf("Jessie’s height is %.0f feet!\n", jessiesNewHeight);
+        	}
+        }
+        else
+        {
+			if(jessiesNewHeightInches == 0.5)
+        	{
+        		System.out.printf("Jessie’s height is %.0f and 1 half feet.\n", jessiesNewHeight);
+        	}
+        	else
+        	{
+        		System.out.printf("Jessie’s height is %.0f feet.\n", jessiesNewHeight);
+        	}
+        }
+        
+    }
+}
