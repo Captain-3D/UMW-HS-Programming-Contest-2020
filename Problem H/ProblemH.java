cfcc5539d9bda47d1190163f5603e10f6c1141f2
Dalton Chichester <dchiches@mail.umw.edu>
2020-01-04 17:29:57 -0500

    Added ProblemH.java as well as Hout1.TXT

---
+++ Problem H/ProblemH.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * The eigth problem in the HS programming constest official solution code.
+ *
+ * @author Dalton Chichester
+ * @version 1.0.0
+ */
+public class ProblemH
+{
+    public static void main(String[] args)
+    {
+    	System.out.println("   ,-==-,");
+    	System.out.println("   \\/\\/\\/");
+    	System.out.println("    \\\\//");
+    	System.out.println("   .-''-.");
+    	System.out.println(" .'.-\"\"-.'.---.");
+    	System.out.println("/ /    .'\\ \\--.'.");
+    	System.out.println("| |   / /| |   \\ \\");
+    	System.out.println("\\ \\  | | / /   | |");
+    	System.out.println(" '.'-:\\ \\.'    / /");
+    	System.out.println("   '---; '-..-'.'");
+    	System.out.println("        `'----'");
+    }
+}
