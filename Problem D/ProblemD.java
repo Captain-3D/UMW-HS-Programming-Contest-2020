148b92e220f84cd8d93ee0ecf7a7f4aca42c9bba
Dalton Chichester <dchiches@mail.umw.edu>
2020-01-04 14:23:28 -0500

    Added ProblemD.java as well as 4 test cases

---
+++ Problem D/ProblemD.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * The fourth problem in the HS programming constest official solution code.
+ *
+ * @author Dalton Chichester
+ * @version 1.0.0
+ */
+public class ProblemD
+{
+    public static void main(String[] args)
+    {
+    	int numBackpacks;
+        double[] backpackWeights;
+        double heaviestBackpack;
+        double lightestBackpack;
+        double sumOfBackpackWeights;
+        double averageBackpackWeight;
+    	Scanner scnr = new Scanner(System.in);
+
+		System.out.print("Please input how many backpacks Jaffy weighed: ");
+        numBackpacks = scnr.nextInt();
+        scnr.nextLine();
+
+        backpackWeights = new double[numBackpacks];
+
+        for(int i = 0; i < numBackpacks; i++)
+        {
+        	int backpackCounter = i+1;
+        	System.out.print("Please input the weight of backpack " + backpackCounter + ": ");
+        	backpackWeights[i] = scnr.nextDouble();
+        	scnr.nextLine();
+        }
+
+        heaviestBackpack = backpackWeights[0];
+        lightestBackpack = backpackWeights[0];
+        sumOfBackpackWeights = backpackWeights[0];
+
+        for(int i = 1; i < numBackpacks; i++)
+        {
+        	if(backpackWeights[i] > heaviestBackpack)
+        	{
+        		heaviestBackpack = backpackWeights[i];
+        	}
+
+        	if(backpackWeights[i] < lightestBackpack)
+        	{
+        		lightestBackpack = backpackWeights[i];
+        	}
+
+        	sumOfBackpackWeights = sumOfBackpackWeights + backpackWeights[i];
+        }
+
+        averageBackpackWeight = sumOfBackpackWeights / numBackpacks;
+
+        if(heaviestBackpack % 1 > .90)
+        {
+        	System.out.printf("Heaviest backpack: %.0f\n", heaviestBackpack);
+        }
+        else
+        {
+        	System.out.printf("Heaviest backpack: %.1f\n", heaviestBackpack);
+        }
+        if(lightestBackpack % 1 > .90)
+        {
+        	System.out.printf("Lightest backpack: %.0f\n", lightestBackpack);
+        }
+        else
+        {
+        	System.out.printf("Lightest backpack: %.1f\n", lightestBackpack);
+        }
+        if(averageBackpackWeight % 1 > .90)
+        {
+        	System.out.printf("Average weight of all backpacks: %.0f\n", averageBackpackWeight);
+        }
+        else
+        {
+        	System.out.printf("Average weight of all backpacks: %.1f\n", averageBackpackWeight);
+        }
+        
+    }
+}
