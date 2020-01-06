bc6eb1fecf63dbe51303f4ba675db482e1da97cf
Dalton Chichester <dchiches@mail.umw.edu>
2020-01-06 15:55:57 -0500

    Added ProblemL.java as well as 5 test cases

---
+++ Problem L/ProblemL.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * The eleventh problem in the HS programming constest official solution code.
+ *
+ * @author Dalton Chichester
+ * @version 1.0.0
+ */
+public class ProblemL
+{
+    public static void main(String[] args)
+    {	
+    	int tankOfWater;
+        String[][] building;
+        int floors;
+        int rooms;
+    	Scanner scnr = new Scanner(System.in);
+
+    	System.out.print("Please input the gallons of water in Jaffys tank: ");
+		tankOfWater = scnr.nextInt();
+        scnr.nextLine();
+
+        System.out.print("Please input the number of floors: ");
+        floors = scnr.nextInt();
+        scnr.nextLine();
+
+        System.out.print("Please input the number of rooms on each floor: ");
+        rooms = scnr.nextInt();
+        scnr.nextLine();
+
+        building = new String[floors][rooms];
+
+        for(int i = 0; i < floors; i++)
+        {
+            System.out.print("Please input the room layout of floor " + (i+1) + ": ");
+            building[i] = scnr.nextLine().split("\\s");;
+        }
+
+        int floorCounter = 0;
+        for(int i = (floors - 1); i > -1; i--)
+        {
+            floorCounter++;
+            for(int j = 0; j < rooms; j++)
+            {
+                if(Integer.parseInt(building[i][j]) > 0)
+                {
+                    if(tankOfWater - Integer.parseInt(building[i][j]) <= 0)
+                    {
+                        System.out.println("The building is still burning!");
+                        System.exit(0);
+                    }
+                    else
+                    {
+                        tankOfWater -= Integer.parseInt(building[i][j]);
+                        System.out.println("Room " + (j+1) + ", Floor " + floorCounter);
+                    }
+                }
+            } 
+        }
+
+        System.out.println("All room fires extinguished.");
+    }
+}
